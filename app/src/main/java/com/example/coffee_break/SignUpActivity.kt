69cc43e7780119b1b1d08a57424310aa8a0e107d
zveri132@gmail.com <zveri132@gmail.com>
2021-11-24 14:53:30 +0500

    sign in2

--- app/src/main/java/com/example/coffee_break/SignUpActivity.kt
+++ app/src/main/java/com/example/coffee_break/SignUpActivity.kt
@@ -2,10 +2,19 @@ package com.example.coffee_break
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatViewInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import recadapters.skid_coffee
 
 class SignUpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(inflater:LayoutInflater,container: ViewGroup, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val root = Inflater.inflate(R.layout.skid_cof,container, false)
+        val rec_coffee : RecyclerView = root.findViewById(R.id.linelay)
+        rec_coffee.adapter = skid_coffee(,Skid_coffee().list)
     }
 }
--- app/src/main/java/recadapters/skid_coffee.kt
+++ app/src/main/java/recadapters/skid_coffee.kt
@@ -4,20 +4,25 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coffee_break.R
 import com.example.coffee_break.coffee
 
 class skid_coffee (val context: Context, val list:ArrayList<coffee>) : RecyclerView.Adapter<skid_coffee.MyVH>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): skid_coffee.MyVH {
-        val root = LayoutInflater.from(context).inflate(R.layout.)
+        val root = LayoutInflater.from(context).inflate(R.layout.skid_cof,parent,false)
+        return MyVH(root)
     }
 class MyVH(itemView: View): RecyclerView.ViewHolder(itemView){
-
+    val name:TextView = itemView.findViewById(R.id.txt_price)
+    val image:ImageView = itemView.findViewById(R.id.img_coffee)
 }
     override fun onBindViewHolder(holder: skid_coffee.MyVH, position: Int) {
-        holder.name.setImageResource(list[position].image)
-        holder.te
+        holder.name.setText(list[position].name)
+        holder.image.setImageResource(list[position].image)
     }
 
     override fun getItemCount(): Int {
